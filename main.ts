717d535391b1537de5897086de61720c61f34aeb
Harish Mohan Raj <hraj@sapient.com>
2017-04-10 10:02:17 +0100

    ts file added

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-alert("not ok!!!")
+import { Observable } from 'rxjs';
+
+let numbers = [1,5,10];
+let source = Observable.from(numbers);
+
+class MyObserver {
+    next(value) {
+        console.log(value)
+    }
+
+    error(e) {
+        console.log("error ${e}")
+    }
+    complete() {
+        console.log("complete!!")
+    }
+}
+source.subscribe(new MyObserver());
